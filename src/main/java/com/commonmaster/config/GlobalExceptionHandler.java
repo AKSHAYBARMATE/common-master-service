cc9ce5cb4d587344ec3e0ae23c5d99a3aae3f20f
AKSHAYBARMATE <100841949+AKSHAYBARMATE@users.noreply.github.com>
2025-10-07 00:03:01 +0530

    Update GlobalExceptionHandler.java

--- src/main/java/com/commonmaster/config/GlobalExceptionHandler.java
+++ src/main/java/com/commonmaster/config/GlobalExceptionHandler.java
@@ -1,27 +1,27 @@
-package com.gateway.config;
+// package com.gateway.config;
 
-import org.springframework.cloud.gateway.config.GlobalCorsProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.reactive.CorsConfiguration;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
+// import org.springframework.cloud.gateway.config.GlobalCorsProperties;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.reactive.CorsConfiguration;
+// import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.cors.reactive.CorsConfigurationSource;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-@Configuration
-public class GatewayCorsConfig {
+// @Configuration
+// public class GatewayCorsConfig {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:92", "http://100.96.183.108"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
+//     @Bean
+//     public CorsConfigurationSource corsConfigurationSource() {
+//         CorsConfiguration config = new CorsConfiguration();
+//         config.setAllowedOrigins(Arrays.asList("http://localhost:92", "http://100.96.183.108"));
+//         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//         config.addAllowedHeader("*");
+//         config.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
-}
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", config);
+//         return source;
+//     }
+// }
